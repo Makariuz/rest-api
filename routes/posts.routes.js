eeffa4949a326d91b47bd227b67e6298f8b06d2d
Makariuz <samuel.eduardo23@hotmail.com>
2022-03-22 19:38:51 +0000

    editing a post + delete

--- routes/posts.routes.js
+++ routes/posts.routes.js
@@ -5,10 +5,8 @@ const router = express.Router()
 
 router.get('/', async (req, res) => {
     const posts = await Post.find()
-    res.send(post)
+    res.send(posts)
 })
-
-
 
 router.post('/', async (req,res) => {
     const {title, content} = req.body
@@ -16,9 +14,29 @@ router.post('/', async (req,res) => {
     res.send(post)
 })
 
+router.get('/:id', async (req,res) => {
+    const {id} = req.params
+    const post = await Post.findById(id)
+    res.send(post)
+})
+
 router.delete('/:id', async (req, res) => {
-    const id = req.params
+    const { id } = req.params
     await Post.findByIdAndDelete(id)
     res.send(`post with id ${id} has been deleted`)
-  })
+})
+
+//patch changes one specific part, like title
+//put changes the whole object, like title && content
+
+router.put('/:id', async (req,res) => {
+    const {title, content} = req.body
+    const {id} = req.params
+    const post = await Post.findById(id)
+    post.title = title
+    post.content = content
+    await post.save()
+    res.send(post)
+})
+
 module.exports = router
