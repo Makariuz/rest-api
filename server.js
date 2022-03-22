aa005ed34b7bd98d58ce45b9e28d2b2d58e971e5
Makariuz <samuel.eduardo23@hotmail.com>
2022-03-22 18:31:00 +0000

    mongodb connected

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require("express")
+const mongoose = require('mongoose')
 const dotenv = require('dotenv')
 dotenv.config()
+
+mongoose.connect(process.env.MONGO_DB_URL)
 
 const app = express();
 
@@ -12,7 +15,8 @@ app.get('/', (req, res) => {
     res.send('hello world')
 })
 
-const postsRoutes = require('./routes/posts.routes')
+const postsRoutes = require('./routes/posts.routes');
+const { default: mongoose } = require("mongoose");
 app.use('/posts', postsRoutes)
 
 app.listen(process.env.PORT)
