00c617c0dfa91db935a2c60e7bc82434370ab9a3
Makariuz <samuel.eduardo23@hotmail.com>
2022-03-22 18:35:00 +0000

    added schema model

---
+++ models/post.model.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose')
+const { Schema, model } = mongoose
+
+const postSchema = new Schema({
+    title: String,
+    content: String,
+})
+
+module.exports = model('Post', postSchema)
